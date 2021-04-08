1a80eeb2920df0111fc3a965be756c082d52639d
Christopher Parks <christopher.parks@ndsu.edu>
2021-04-08 13:01:42 -0500

    Revert to previous build shell script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-set echo off
-rm -f *.pyz
-cp -r ./game ./wrapper/game
-cp -r  ./scrimmage ./wrapper/scrimmage
-python3 -m zipapp  ./wrapper -o ./launcher.pyz -c
-rm -rf ./wrapper/game
-rm -rf ./wrapper/scrimmage
+#!/bin/bash
+
+rm *.pyz; 
+
+cp -rf game wrapper/
+python3 -m zipapp wrapper -c -p "/usr/bin/env python3" -o launcher.pyz; 
+rm -rf wrapper/game
