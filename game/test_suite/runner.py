b0487bf70ef7c14e3bc7d8230254c71a2e441abe
unknown <erickbickler@gmail.com>
2021-05-22 20:20:46 -0500

    added tests to init

--- game/test_suite/runner.py
+++ game/test_suite/runner.py
@@ -13,5 +13,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-# To run the test suite, make sure your terminal is in the root directory of the project (the 'byte_le_royale_2021 folder)
+# To run the test suite, make sure your terminal is in the root directory of the project (the 'byte_le_royale_2022 folder)
 # Then, in your terminal, run 'python -m game.test_suite.runner'. This runs this file as a module of the entire project, allowing imports to function properly
