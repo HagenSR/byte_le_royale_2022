3d45a51a7e5c02c42ff0b994f21c5c944548a14f
Erick Bickler <60521336+erickbickler@users.noreply.github.com>
2021-05-23 16:41:24 -0500

    Add game board (#6)
    * added game board and stats file
    
    * fixed imports
    
    * added obfuscate method
    
    * added lethal list
    
    * added unit tests
    
    * added lethal list to json methods
    
    * add logic for test based on game stats
    
    * added tests to init

--- game/test_suite/tests/__init__.py
+++ game/test_suite/tests/__init__.py
@@ -1,8 +1,8 @@
 # When you create a new test file, make sure to add it here.
 # Simply import the class from your file, and then add that class to the '__all__' array.
 
-from game.test_suite.tests.test_example import TestExample
+from game.test_suite.tests.test_game_board import TestGameBoard
 
 __all__ = [
-    'TestExample'
+    'TestGameBoard'
 ]
