b004cbfd11e4420482fb05148ee2cbc3a3d01b53
Christopher Parks <christopher.parks@ndsu.edu>
2021-05-23 17:49:42 -0500

    added collidable property to map object

--- game/common/map_object.py
+++ game/common/map_object.py
@@ -2,20 +2,23 @@ from game.common.game_object import GameObject
 from game.common.enums import *
 
 class MapObject(GameObject):
-    def __init__(self, map_object_type=None, health=None, coordinates=None, hitbox=None):
+    def __init__(self, map_object_type=None, health=None, coordinates=None, hitbox=None, collidable=None):
         super().__init__()
         self.health = health
         self.coordinates = coordinates
         self.hitbox = hitbox
+        self.collidable = collidable
     
     def to_json(self):
         data = super().__init__()
         data['health'] = self.health
         data['coordinates'] = self.coordinates
         data['hitbox'] = self.hitbox
+        data['collidable'] = self.collidable
 
     def from_json(self, data):
         self.health = data['health']
         self.coordinates = data['coordinates']
         self.hitbox = data['hitbox']
+        self.collidable = data['collidable']
 
