1fb19dc59ab6a14cb20e1b15743013915429ab38
Christopher Parks <55614090+Anomalous-Creator@users.noreply.github.com>
2021-05-24 08:52:06 -0500

    Game object (#7)
    * added game board and stats file
    
    * fixed imports
    
    * added obfuscate method
    
    * added lethal list
    
    * added unit tests
    
    * added lethal list to json methods
    
    * add logic for test based on game stats
    
    * initial map objects
    
    * removed unnecessary changes
    
    * added collidable property to map object
    
    * fixed mistakes, old implementation
    
    Co-authored-by: unknown <erickbickler@gmail.com>
    Co-authored-by: Christopher Parks <christopher.parks@ndsu.edu>

---
+++ game/common/map_object.py
@@ -0,0 +1,27 @@
+from game.common.game_object import GameObject
+from game.common.enums import *
+
+class MapObject(GameObject):
+    def __init__(self, map_object_type=None, health=None, coordinates=None, hitbox=None, collidable=None):
+        super().__init__()
+        self.health = health
+        self.coordinates = coordinates
+        self.hitbox = hitbox
+        self.collidable = collidable
+    
+    def to_json(self):
+        data = super().to_json()
+        data['health'] = self.health
+        data['coordinates'] = self.coordinates
+        data['hitbox'] = self.hitbox
+        data['collidable'] = self.collidable
+
+        return data
+
+    def from_json(self, data):
+        super().from_json(data)
+        self.health = data['health']
+        self.coordinates = data['coordinates']
+        self.hitbox = data['hitbox']
+        self.collidable = data['collidable']
+
--- game/common/stats.py
+++ game/common/stats.py
@@ -3,3 +3,9 @@
 class GameStats:
     game_board_width = 500
     game_board_height = 500
+
+    player_stats = {
+        'starting_health': 10,
+        'starting_coordinates': [{'x': 450, 'y': 450}, {'x': 50, 'y': 50}],
+        'hitbox': {'width': 10, 'height': 10}
+    }
