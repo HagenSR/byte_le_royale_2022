5b1cbf6284d01280a0736023c8d18fb3d45d6db6
Mitchell-Borders <mitchell.borders28@gmail.com>
2021-05-27 16:10:40 -0500

    deleted extra spaces

--- game/common/walls.py
+++ game/common/walls.py
@@ -1,7 +1,5 @@
-
 from game.common.map_object import MapObject
 from game.common.stats import GameStats
-
 
 class Walls(MapObject):
     def __init__(self, coordinates, hitbox, health = GameStats.default_wall_health, destructible = False):
@@ -16,5 +14,3 @@ class Walls(MapObject):
     def from_json(self, data):
         super().from_json(data)
         self.destructible = data['destructible']
-    
-        
