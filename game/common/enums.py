16e628ac276913f2fbd7e06310bf7a3a98a3b58f
Christopher Parks <55614090+Anomalous-Creator@users.noreply.github.com>
2021-05-30 01:20:12 +0000

    Create gun object (#28)
    * added game board and stats file
    
    * fixed imports
    
    * added obfuscate method
    
    * added lethal list
    
    * added unit tests
    
    * added lethal list to json methods
    
    * add logic for test based on game stats
    
    * initial map objects
    
    * removed unnecessary changes
    
    * added collidable property to map object
    
    * fixed mistakes, old implementation
    
    * unnecessary parameter removed
    
    * Created gun object, enums, stats, to string
    
    * added pattern, missing cooldown
    
    * added level, inheritance from item
    
    * added object types
    
    * moved gun object, added object types
    
    * removed .vs dir
    
    Co-authored-by: unknown <erickbickler@gmail.com>
    Co-authored-by: Christopher Parks <christopher.parks@ndsu.edu>

--- game/common/enums.py
+++ game/common/enums.py
@@ -10,3 +10,25 @@ class ObjectType:
     action = 1
     player = 2
     game_board = 3
+    map_object = 4
+    item = 5
+    gun = 6
+
+class GunType:
+    none = 0
+    handgun = 1
+    assault_rifle = 2
+    shotgun = 3
+    sniper = 4
+
+class GunLevel:
+    level_zero = 0
+    level_one = 1
+    level_two = 2
+    level_three = 3
+
+class ShotPattern:
+    none = 0
+    single = 1
+    multi = 2
+    spread = 3
--- game/common/items/item.py
+++ game/common/items/item.py
@@ -3,6 +3,7 @@ from game.common.map_object import MapObject
 class Item(MapObject):
     def __init__(self, coordinates, hitbox, health=None, count = 1):
         super().__init__(health, coordinates, hitbox, True)
+        self.object_type = ObjectType.item
         self.count = count
     
     def to_json(self):
