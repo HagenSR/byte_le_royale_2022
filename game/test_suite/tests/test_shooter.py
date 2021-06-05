03ef6e28bb81fc321deb87ee5f270592b81e9b45
erickbickler <erickbickler@gmail.com>
2021-06-05 15:11:42 -0500

    added new tests

--- game/test_suite/tests/test_shooter.py
+++ game/test_suite/tests/test_shooter.py
@@ -3,21 +3,19 @@ from game.common.moving.shooter import Shooter
 from game.common.items.gun import Gun
 from game.common.items.upgrade import Upgrade
 from game.common.items.consumable import Consumable
+from game.common.items.item import Item
 from game.common.enums import *
+from game.common.errors.inventory_full_error import InventoryFullError
 
 
 class TestShooter(unittest.TestCase):
     def setUp(self) -> None:
         self.shooter = Shooter()
 
-    def test_inventory(self):
+    def test_inventory_guns(self):
         self.assertTrue(self.shooter.has_empty_slot('guns'))
-        self.assertTrue(self.shooter.has_empty_slot('upgrades'))
-        self.assertTrue(self.shooter.has_empty_slot('consumables'))
 
         test_gun = Gun(GunType.shotgun, 1)
-        test_upgrade = Upgrade()
-        test_consumable = Consumable()
 
         for slot in self.shooter.inventory['guns']:
             self.shooter.append_inventory(test_gun)
@@ -25,11 +23,21 @@ class TestShooter(unittest.TestCase):
         self.assertEqual(test_gun, self.shooter.remove_from_inventory(test_gun))
         self.assertTrue(self.shooter.has_empty_slot('guns'))
 
+    def test_inventory_upgrades(self):
+        self.assertTrue(self.shooter.has_empty_slot('upgrades'))
+
+        test_upgrade = Upgrade()
+
         for slot in self.shooter.inventory['upgrades']:
             self.shooter.append_inventory(test_upgrade)
         self.assertFalse(self.shooter.has_empty_slot('upgrades'))
         self.assertIsNotNone(self.shooter.remove_from_inventory(test_upgrade))
         self.assertTrue(self.shooter.has_empty_slot('upgrades'))
+
+    def test_inventory_consumables(self):
+        self.assertTrue(self.shooter.has_empty_slot('consumables'))
+
+        test_consumable = Consumable()
 
         for slot in self.shooter.inventory['consumables']:
             self.shooter.append_inventory(test_consumable)
@@ -37,3 +45,11 @@ class TestShooter(unittest.TestCase):
         self.assertEqual(test_consumable, self.shooter.remove_from_inventory(test_consumable))
         self.assertTrue(self.shooter.has_empty_slot('consumables'))
 
+    def test__append_inventory__wrong_inventory_type_raises_TypeError(self):
+        self.assertRaises(InventoryFullError, self.shooter.append_inventory(Item(None, None)))
+
+    def test__append_inventory__correct_type_inventory_full__raises_InventoryFullError(self):
+        test_gun = Gun(GunType.shotgun, 1)
+        for slot in self.shooter.inventory['guns']:
+            self.shooter.append_inventory(test_gun)
+        self.assertRaises(InventoryFullError, self.shooter.append_inventory(test_gun))
