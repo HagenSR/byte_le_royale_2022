e766f758569bbc4b2390ecd5361f8545547ff141
Mitchell-Borders <mitchell.borders28@gmail.com>
2021-06-11 18:52:24 -0500

    Passed arguments to consumable constructor in test_shooter. Took out spawnable boolean. When I tried running tests they wouldn't run unless I had a something set up, so thats what the .vscode/settings.json is.

--- game/common/items/consumable.py
+++ game/common/items/consumable.py
@@ -3,23 +3,20 @@ from game.common.enums import Consumables
 
 
 class Consumable(Item):
-    def __init__(self, coordinates, hitbox, health, count, spawnable = True, speed_enum = None, health_enum = None, armor_enum = None):
+    def __init__(self, coordinates, hitbox, health, count, speed_enum = None, health_enum = None, armor_enum = None):
         super().__init__(coordinates, hitbox, health, count)
-        self.spawnable = spawnable
         self.speed_enum = speed_enum
         self.health_enum = health_enum
         self.armor_enum = armor_enum
         
     def to_json(self):
         data = super().to_json()
-        data['spawnable'] = self.spawnable
         data['speed_enum'] = self.speed_enum
         data['health_enum'] = self.health_enum
         data['armor_enum'] = self.armor_enum
         
     def from_json(self, data):
         super().from_json(data)
-        self.spawnable = data['spawnable']
         self.speed_enum = data['speed_enum']
         self.health_enum = data['health_enum']
         self.armor_enum = data['armor_enum']
