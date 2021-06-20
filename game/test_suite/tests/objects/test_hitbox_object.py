9409ea63dea2bb6e85111fb4952f712358d57a82
Sean Hagen <sean.r.hagen@gmail.com>
2021-06-19 20:04:01 -0500

    Fixed error in corner property. Added more unit tests

--- game/test_suite/tests/objects/test_hitbox_object.py
+++ game/test_suite/tests/objects/test_hitbox_object.py
@@ -7,7 +7,7 @@ from game.common.hitbox import Hitbox
 
 class TestHitboxObject(unittest.TestCase):
     def setUp(self):
-       self.hitbox = Hitbox(5,5,(5,5))
+       self.hitbox = Hitbox(2,2,(0,0))
     
     def test_set_get_heigth_valid(self):
         self.hitbox.height = 10
@@ -17,25 +17,81 @@ class TestHitboxObject(unittest.TestCase):
         self.hitbox.height = 7
         self.assertEqual(self.hitbox.height, 7)
 
-    def test_set_get_height_invalid_low(self):
-        # Checks if an exception is raised by an Illegal set
-        # Lambda is needed because the property set isn't considered a function (which assertRaises takes as an argument)
-        self.assertRaises(Exception, lambda : {self.hitbox.height(10)})
-
-    def test_set_get_width_invalid_low(self):
-        self.assertRaises(Exception, lambda : self.hitbox.width(10))
+    def test_set_get_tuple_x_valid(self):
+        self.hitbox.position = (50,10)
+        self.assertEqual(self.hitbox.position, (50,10))
     
-    def test_set_get_tuple_x_boundary_low(self):
-        self.assertRaises(Exception, lambda : self.hitbox.position((10, 10)))
+    def test_set_get_tuple_y_valid(self):
+        self.hitbox.position = (10,50)
+        self.assertEqual(self.hitbox.position, (10,50))
 
     def test_set_get_tuple_y_boundary_low(self):
         self.hitbox.position = (10,0)
-        self.assertEqual(self.hitbox.position[0], 10)
-        self.assertEqual(self.hitbox.position[1], 0)
+        self.assertEqual(self.hitbox.position, (10,0))
+
+    def test_set_get_tuple_x_boundary_high(self):
+        self.hitbox.position = (GameStats.game_board_width,10)
+        self.assertEqual(self.hitbox.position, (GameStats.game_board_width,10))
+
+    # def test_set_get_tuple_X_invalid_high(self):
+    #     self.hitbox.position = (10,GameStats.game_board_height)
+    #     self.assertEqual(self.hitbox.position, (10,GameStats.game_board_height))
+
+    # def test_set_get_tuple_y_invalid_high(self):
+    #     self.hitbox.position = (10,GameStats.game_board_height)
+    #     self.assertEqual(self.hitbox.position, (10,GameStats.game_board_height))
+
+    def test_topLeft_corner(self):
+        self.assertEqual(self.hitbox.topLeft, (-1,1))
+
+    def test_topLeft_corner(self):
+        self.assertEqual(self.hitbox.topRight, (1,1))
+
+    def test_topLeft_corner(self):
+        self.assertEqual(self.hitbox.bottomLeft, (-1,-1))
+
+    def test_topLeft_corner(self):
+        self.assertEqual(self.hitbox.bottomRight, (1,-1))
+
+    def test_topLeft_corner_alt(self):
+        self.hitbox.position = (10,10) 
+        self.assertEqual(self.hitbox.topLeft, (9,11))
+
+    def test_topLeft_corner_alt(self):
+        self.hitbox.position = (10,10) 
+        self.assertEqual(self.hitbox.topRight, (11,11))
+
+    def test_topLeft_corner_alt(self):
+        self.hitbox.position = (10,10) 
+        self.assertEqual(self.hitbox.bottomLeft, (9,9))
+
+    def test_topLeft_corner_alt(self):
+        self.hitbox.position = (10,10) 
+        self.assertEqual(self.hitbox.bottomRight, (11,9))
 
     def test_damaging_obj_parent_params(self):
         self.assertIsNotNone(self.hitbox.id)
         self.assertIsNotNone(self.hitbox.object_type)
+
+    
+    # Needs a rework
+
+    # def test_set_get_height_invalid_low(self):
+    #     # Checks if an exception is raised by an Illegal set
+    #     # Lambda is needed because the property set isn't considered a function (which assertRaises takes as an argument)
+    #     self.assertRaises(Exception, lambda : {self.hitbox.height(10)})
+
+    # def test_set_get_width_invalid_low(self):
+    #     self.assertRaises(Exception, lambda : self.hitbox.width(10))
+    
+    # def test_set_get_tuple_x_boundary_low(self):
+    #     self.assertRaises(Exception, lambda self.hitbox.position((10, 10)))
+
+    # def test_set_get_tuple_x_invalid_high(self):
+    # try:
+    #     self.hitbox.position = (GameStats.game_board_width,10)
+    # except:
+    #     self.assertTrue(True)
     
 if __name__ == '__main__':
      unittest.main 
