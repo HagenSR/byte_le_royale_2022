a71a328e7cd16aa563283332b7ca7f9bdc16fbf2
amanda-f-ndsu <71937870+amanda-f-ndsu@users.noreply.github.com>
2021-06-29 14:57:49 -0500

    made name changes and revised algorithm; added additional unit tests.

--- game/test_suite/tests/collision_test.py
+++ game/test_suite/tests/collision_test.py
@@ -1,38 +1,76 @@
-import unittest 
+import unittest
 from game.common.hitbox import Hitbox
-from game.utils.collision_detection import checkCollision
+from game.utils.collision_detection import check_collision
+
 
 class TestCollision(unittest.TestCase):
-    
+
     def setUp(self):
-       self.hitOne = Hitbox(10,10,(5,5))
-       self.hitTwo = Hitbox(25,25,(10,10))
+        self.hitOne = Hitbox(10, 10, (5, 5))
+        self.hitTwo = Hitbox(25, 25, (5, 6))
 
-    
-    def test_collision_true(self):
-        self.hitOne.position = (5.0,5.0)
-        self.hitOne.height = 5
-        self.hitOne.width = 8
-  
-        self.hitTwo.position = (5.0,6.0)
-        self.hitTwo.height = 5
-        self.hitTwo.width = 8
-
-
-        self.assertTrue(checkCollision(self.hitOne,self.hitTwo))
-    
-    print(hitOne.topLeft)
-
-    def test_collision_false(self):
-        self.hitOne.position = (5,5)
+    """Below are 3 examples of true cases and false cases. The hit boxes were graphed out beforehand 
+    in order to determine if the test cases are behaving as expected."""
+    def test_collision_true_one(self):
+        self.hitOne.position = (5, 5)
         self.hitOne.height = 5
         self.hitOne.width = 8
 
-        self.hitTwo.position = (20,20)
+        self.hitTwo.position = (5, 6)
         self.hitTwo.height = 5
         self.hitTwo.width = 8
+        self.assertTrue(check_collision(self.hitOne, self.hitTwo))
 
-        self.assertFalse(checkCollision(self.hitOne,self.hitTwo))
+    def test_collision_false_one(self):
+        self.hitOne.position = (5, 5)
+        self.hitOne.height = 5
+        self.hitOne.width = 8
+
+        self.hitTwo.position = (20, 20)
+        self.hitTwo.height = 5
+        self.hitTwo.width = 8
+        self.assertFalse(check_collision(self.hitOne, self.hitTwo))
+
+    def test_collision_true_two(self):
+        self.hitOne.position = (5, 5)
+        self.hitOne.height = 6
+        self.hitOne.width = 4
+
+        self.hitTwo.position = (1, 3)
+        self.hitTwo.height = 2
+        self.hitTwo.width = 6
+        self.assertTrue(check_collision(self.hitOne, self.hitTwo))
+
+    def test_collision_false_two(self):
+        self.hitOne.position = (5, 5)
+        self.hitOne.height = 4
+        self.hitOne.width = 4
+
+        self.hitTwo.position = (20, 20)
+        self.hitTwo.height = 6
+        self.hitTwo.width = 2
+        self.assertFalse(check_collision(self.hitOne, self.hitTwo))
+
+    def test_collision_true_three(self):
+        self.hitOne.position = (60, 50)
+        self.hitOne.height = 40
+        self.hitOne.width = 80
+
+        self.hitTwo.position = (20, 20)
+        self.hitTwo.height = 30
+        self.hitTwo.width = 40
+        self.assertTrue(check_collision(self.hitOne, self.hitTwo))
+
+    def test_collision_false_three(self):
+        self.hitOne.position = (60, 50)
+        self.hitOne.height = 40
+        self.hitOne.width = 80
+
+        self.hitTwo.position = (20, 20)
+        self.hitTwo.height = 10
+        self.hitTwo.width = 20
+        self.assertFalse(check_collision(self.hitOne, self.hitTwo))
+
 
 if __name__ == '__main__':
-     unittest.main 
+    unittest.main
--- game/utils/collision_detection.py
+++ game/utils/collision_detection.py
@@ -2,15 +2,12 @@ from game.common.hitbox import Hitbox
 from game.common.stats import GameStats
 from game.common.enums import *
 
-def checkCollision(hitbox_one, hitbox_two):
-    if (hitbox_one.topLeft[0] < hitbox_two.topRight[0] and 
-            hitbox_one.topRight[0] > hitbox_two.topLeft[0] and 
-            hitbox_one.topLeft[1] < hitbox_two.bottomLeft[1] and 
-            hitbox_one.bottomRight[1] > hitbox_two.topRight[1]):
-        
-        # if (RectA.X1 < RectB.X2 && RectA.X2 > RectB.X1 &&
-   # RectA.Y1 < RectB.Y2 && RectA.Y2 > RectB.Y1) 
+
+def check_collision(hitbox_one, hitbox_two):
+    if (hitbox_one.topLeft[0] < hitbox_two.topRight[0] and
+            hitbox_one.topRight[0] > hitbox_two.topLeft[0] and
+            hitbox_one.topLeft[1] > hitbox_two.bottomLeft[1] and
+            hitbox_one.bottomRight[1] < hitbox_two.topRight[1]):
         return True
     else:
         return False
-  
