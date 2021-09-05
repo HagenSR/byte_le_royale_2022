dc7833a5f219f24e654baa0c444db4fd814483f1
erickbickler <erickbickler@gmail.com>
2021-09-05 01:37:21 -0500

    add to master controller

--- game/controllers/master_controller.py
+++ game/controllers/master_controller.py
@@ -5,6 +5,7 @@ from game.common.action import Action
 from game.common.enums import *
 from game.common.player import Player
 import game.config as config
+from game.controllers.player_view_controller import PlayerViewController
 from game.utils.threadBytel import CommunicationThread
 
 from game.controllers.controller import Controller
@@ -19,7 +20,8 @@ class MasterController(Controller):
 
         self.current_world_data = None
 
-        self.boundry_controller = KillBoundaryController()
+        self.boundary_controller = KillBoundaryController()
+        self.player_view_controller = PlayerViewController()
 
     # Receives all clients for the purpose of giving them the objects they
     # will control
@@ -59,8 +61,10 @@ class MasterController(Controller):
 
     # Perform the main logic that happens per turn
     def turn_logic(self, clients, turn):
-        self.boundry_controller.handle_actions(
+        self.boundary_controller.handle_actions(
             clients, self.current_world_data["game_map"].circle_radius)
+
+        self.player_view_controller.handle_actions(clients, self.current_world_data["game_map"])
 
         for client in clients:
             ReloadController.handle_actions(client)
