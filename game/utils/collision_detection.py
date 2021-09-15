c73d0a21222cf724ce26385460a186bc22c30e1a
erickbickler <erickbickler@gmail.com>
2021-09-15 01:41:14 -0500

    make fix for undefined and 0 slopes of edges, add unit tests

--- game/utils/collision_detection.py
+++ game/utils/collision_detection.py
@@ -24,16 +24,16 @@ def arc_intersect_rect(center, radius, arc_len_degree, hitbox, heading):
 
 
 def point_in_hitbox(x, y, hitbox):
-    return (hitbox.bottomLeft[0] < x < hitbox.topRight[0] and
-            hitbox.bottomLeft[1] < y < hitbox.topRight[1])
+    return (hitbox.bottomLeft[0] <= x <= hitbox.topRight[0] and
+            hitbox.topRight[1] <= y <= hitbox.bottomLeft[1])
 
 
 def intersect_arc(center, radius, hitbox, heading, arc_len_deg):
     edges = [
-        [hitbox.topleft, hitbox.topright],
+        [hitbox.topLeft, hitbox.topRight],
         [hitbox.topRight, hitbox.bottomRight],
-        [hitbox.bottomRight, hitbox.bottomLeft],
-        [hitbox.bottomLeft, hitbox.topLeft]
+        [hitbox.bottomLeft, hitbox.bottomRight],
+        [hitbox.topLeft, hitbox.bottomLeft]
     ]
     x3 = center[0]
     y3 = center[1]
@@ -43,23 +43,33 @@ def intersect_arc(center, radius, hitbox, heading, arc_len_deg):
         y1 = edge[0][1]
         y2 = edge[1][1]
 
-        # calculate x coord of the intercept of the edge and the radius perpendicular to the edge
-        # numerator
-        xin = (y3 * (x2 - x1) - ((x2 - x1) ** 2 / y1 - y2)
-               * x3 - (x1 * y2 - x2 * y1))
-        # denominator
-        xid = ((y1 - y2) - ((x2 - x1) ** 2 / (y1 - y2)))
-        xi = xin / xid
+        # check for edge being vertical
+        if x1 == x2:
+            xi = x1
+            yi = y3
+            dx4 = math.nan
+        # check for edge being horizontal
+        elif y1 == y2:
+            xi = x3
+            yi = y1
+            dx4 = 0
+        else:
+            # calculate x coord of the intercept of the edge and the radius perpendicular to the edge
+            # numerator
+            xin = (y3 * (x2 - x1) - ((x2 - x1) ** 2 / y1 - y2) * x3 - (x1 * y2 - x2 * y1))
+            # denominator
+            xid = ((y1 - y2) - ((x2 - x1) ** 2 / (y1 - y2)))
+            xi = xin / xid
 
-        # calculate y coord of the intercept of the edge and the radius
-        # perpendicular to the edge
-        yi = ((x2 - x1) / (y1 - y2)) * x3 - y3 - ((x2 - x1) / (y1 - y2)) * xi
+            # calculate y coord of the intercept of the edge and the radius
+            # perpendicular to the edge
+            yi = ((x2 - x1) / (y1 - y2)) * x3 - y3 - ((x2 - x1) / (y1 - y2)) * xi
 
-        dx4 = -1 * ((y1 - y2) / (x2 - x1))
+            dx4 = -1 * ((y1 - y2) / (x2 - x1))
 
         # calculate length of perpendicular line segment from radius to the
         # edge
-        seg_len = ((xi - y3) ** 2 + (yi - y3) ** 2) ** (1 / 2)
+        seg_len = distance(xi, yi, x3, y3)
 
         d_arc1 = math.tan(heading + arc_len_deg / 2)
         d_arc2 = math.tan(heading - arc_len_deg / 2)
@@ -69,7 +79,14 @@ def intersect_arc(center, radius, hitbox, heading, arc_len_deg):
         # need to also check that the intersect point is actually between the two endpoints of the edge
         # and to adjust for the arc, need to check the slope of the perp. line is between the two slopes of the bounding
         # lines of the arc
-        if seg_len < radius and x1 <= xi <= x2 and y1 <= yi <= y2 and d_arc1 < dx4 < d_arc2:
+        if (seg_len < radius and
+                ((distance(x1, y1, x3, y3) < radius or distance(x2, y2, x3, y3) < radius) or
+                 x1 <= xi <= x2 and y1 <= yi <= y2)):
+            # TODO add check for arc, currently only checks circle
             return True
 
     return False
+
+
+def distance(x1, y1, x2, y2):
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
