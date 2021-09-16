39df5d1dc8e55c1d2c5edaed562e31c62b552448
erickbickler <erickbickler@gmail.com>
2021-09-16 14:15:01 -0500

    movement controller cleanup

--- game/controllers/movement_controller.py
+++ game/controllers/movement_controller.py
@@ -1,11 +1,7 @@
 import math
 
-from game.common.hitbox import Hitbox
 from game.controllers.controller import Controller
 from game.utils.calculate_new_location import calculate_location
-from game.common.moving.shooter import Shooter
-from game.common.action import Action
-from game.common.game_board import GameBoard
 from game.common.enums import *
 
 
@@ -21,7 +17,6 @@ class MovementController(Controller):
         # If statement for if client chooses move action
         if client.action.chosen_action is ActionType.move:
             # shooter object is removed from old location on gameboard to avoid object duplicates
-            # breakpoint()
             world["game_board"].partition.remove_object(client.shooter)
             # variable for client's location prior to movement
             location = client.shooter.hitbox.position
@@ -31,17 +26,15 @@ class MovementController(Controller):
             angle = client.shooter.heading
             # new location is calculated using utils method
             target_location = calculate_location(location, speed, angle)
-            # print(target_location)
             self.space_free = True
-            while (location != target_location and self.space_free):
-                if (world["game_board"].partition.find_object_hitbox(client.shooter.hitbox) == False):
+            while location != target_location and self.space_free:
+                if not world["game_board"].partition.find_object_hitbox(client.shooter.hitbox):
                     new_x = location[0] + math.cos(angle)
                     new_y = location[1] + math.sin(angle)
                     client.shooter.hitbox.position = (new_x, new_y)
-                    if (abs(new_x - target_location[0]) < 0.00001 and abs(new_y - target_location[1]) < 0.00001):
+                    if abs(new_x - target_location[0]) < 0.00001 and abs(new_y - target_location[1]) < 0.00001:
                         client.shooter.hitbox.position = target_location
                     location = client.shooter.hitbox.position
-                    # print(location)
                 else:
                     self.space_free = False
             # gameboard is updated with new client location
