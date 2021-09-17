8941a01f5e04eb1138f118e2568babf09c80c3fc
Mitchell-Borders <78919160+Mitchell-Borders@users.noreply.github.com>
2021-09-16 19:59:34 -0500

    Updated parameter to be "curr_tick" instead of "turn"

--- game/controllers/loot_generation_controller.py
+++ game/controllers/loot_generation_controller.py
@@ -6,16 +6,16 @@ from game.utils.item_gen_utils import placeItems
 
 class LootGenerationController(Controller):
 
-    def handle_actions(self, game_board, turn, kill_boundry_radius):
-        if turn is GameStats.initial_wave:
+    def handle_actions(self, game_board, curr_tick, kill_boundry_radius):
+        if curr_tick is GameStats.initial_wave:
             number_items = random.randrange(10, 30, 1)
             placeItems(game_board, 1, kill_boundry_radius, number_items)
-        if turn is GameStats.loot_wave_1_tick:
+        if curr_tick is GameStats.loot_wave_1_tick:
             number_items = random.randrange(10, 20, 1)
             placeItems(game_board, 2, kill_boundry_radius, number_items)
-        if turn is GameStats.loot_wave_2_tick:
+        if curr_tick is GameStats.loot_wave_2_tick:
             number_items = random.randrange(5, 15, 1)
             placeItems(game_board, 3, kill_boundry_radius, number_items)
-        if turn is GameStats.loot_wave_3_tick:
+        if curr_tick is GameStats.loot_wave_3_tick:
             number_items = random.randrange(5, 10, 1)
             placeItems(game_board, 4, kill_boundry_radius, number_items)
