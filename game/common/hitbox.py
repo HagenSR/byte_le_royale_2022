fecf70bf31de76908620330cb9902677fee20182
erickbickler <erickbickler@gmail.com>
2021-09-16 23:37:41 -0500

    add calculations to account for diagonal hitboxes

--- game/common/hitbox.py
+++ game/common/hitbox.py
@@ -1,16 +1,32 @@
+import math
+
 from game.common.game_object import GameObject
 from game.common.enums import *
 import game.common.stats as stats
 
 
 class Hitbox(GameObject):
-    def __init__(self, width, height, xy_tuple):
+    def __init__(self, width, height, xy_tuple, rotation=0):
         super().__init__()
         self.object_type = ObjectType.hitbox
         self.width = width
         self.height = height
         # (x,y) tuple, where [0] is the x position and y is [1] of the top left corner
         self.position = xy_tuple
+        # added rotation to allow for diagonal hitboxes while keeping backwards compatibility
+        self.rotation = math.radians(rotation)
+
+        self.__calculate_positions()
+
+    # method to be called anytime positions should change
+    def __calculate_positions(self):
+        # use backing variables so the equations don't get calculated everytime the property gets called
+        self.__topRight = (self.position[0] + (self.width * math.cos(self.rotation)),
+                           self.position[1] + (self.width * math.sin(self.rotation)))
+        self.__bottomLeft = (self.position[0] + (self.height * math.sin(self.rotation)),
+                             self.position[1] + (self.height * math.cos(self.rotation)))
+        self.__bottomRight = (self.__bottomLeft[0] + (self.width * math.cos(self.rotation)),
+                              self.__bottomLeft[1] + (self.width * math.sin(self.rotation)))
 
     @property
     def width(self):
@@ -30,15 +46,15 @@ class Hitbox(GameObject):
 
     @property
     def topRight(self):
-        return (self.position[0] + self.width, self.position[1])
+        return self.__topRight
 
     @property
     def bottomLeft(self):
-        return (self.position[0], self.position[1] + self.height)
+        return self.__bottomLeft
 
     @property
     def bottomRight(self):
-        return (self.position[0] + self.width, self.position[1] + self.height)
+        return self.__bottomRight
 
     @property
     def middle(self):
@@ -55,6 +71,7 @@ class Hitbox(GameObject):
     def height(self, val):
         if val > 0:
             self.__height = val
+            self.__calculate_positions()
         else:
             raise ValueError("Tried to set an invalid height for hitbox")
 
@@ -63,6 +80,7 @@ class Hitbox(GameObject):
     def width(self, val):
         if val > 0:
             self.__width = val
+            self.__calculate_positions()
         else:
             raise ValueError("Tried to set an invalid width for hitbox")
 
@@ -72,6 +90,7 @@ class Hitbox(GameObject):
         if (0 <= val[0] <= stats.GameStats.game_board_width) and (
                 0 <= val[1] <= stats.GameStats.game_board_height):
             self.__position = val
+            self.__calculate_positions()
         else:
             raise ValueError(
                 "Tried to set an invalid xy position tuple for hitbox")
