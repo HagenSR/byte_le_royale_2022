6734e857b04473f5ed8c1d7d8034209e680e7fa5
erickbickler <erickbickler@gmail.com>
2021-10-06 22:38:56 -0500

    add more to minkowski arc intersection

--- game/utils/collision_detection.py
+++ game/utils/collision_detection.py
@@ -1,7 +1,4 @@
 import math
-from game.common.hitbox import Hitbox
-from game.common.stats import GameStats
-from game.common.enums import *
 
 
 def check_collision(hitbox_one, hitbox_two):
@@ -28,7 +25,7 @@ def point_in_hitbox(x, y, hitbox):
             hitbox.topRight[1] <= y <= hitbox.bottomLeft[1])
 
 
-def intersect_arc1(center, radius, hitbox, heading, arc_len_deg):
+def intersect_circle(center, radius, hitbox):
     edges = [
         [hitbox.topLeft, hitbox.topRight],
         [hitbox.topRight, hitbox.bottomRight],
@@ -67,14 +64,9 @@ def intersect_arc1(center, radius, hitbox, heading, arc_len_deg):
             yi = ((x2 - x1) / (y1 - y2)) * x3 - \
                 y3 - ((x2 - x1) / (y1 - y2)) * xi
 
-            dx4 = -1 * ((y1 - y2) / (x2 - x1))
-
         # calculate length of perpendicular line segment from radius to the
         # edge
         seg_len = distance(xi, yi, x3, y3)
-
-        d_arc1 = math.tan(heading + arc_len_deg / 2)
-        d_arc2 = math.tan(heading - arc_len_deg / 2)
 
         # if the line segment from the radius perpendicular to the edge is less than the total length of the radius,
         # the rectangle intercepts with the rectangle
@@ -83,69 +75,6 @@ def intersect_arc1(center, radius, hitbox, heading, arc_len_deg):
         # lines of the arc
         if (seg_len < radius and ((distance(x1, y1, x3, y3) < radius or distance(
                 x2, y2, x3, y3) < radius) or x1 <= xi <= x2 and y1 <= yi <= y2)):
-            # TODO add check for arc, currently only checks circle
-
-            return True
-
-    return False
-
-
-def intersect_arc(center, radius, hitbox, heading, arc_len_deg):
-    edges = [
-        [hitbox.topLeft, hitbox.topRight],
-        [hitbox.topRight, hitbox.bottomRight],
-        [hitbox.bottomLeft, hitbox.bottomRight],
-        [hitbox.topLeft, hitbox.bottomLeft]
-    ]
-    x3 = center[0]
-    y3 = center[1]
-    for edge in edges:
-        x1 = edge[0][0]
-        x2 = edge[1][0]
-        y1 = edge[0][1]
-        y2 = edge[1][1]
-
-        # check for edge being vertical
-        if x1 == x2:
-            xi = x1
-            yi = y3
-            dx4 = math.nan
-        # check for edge being horizontal
-        elif y1 == y2:
-            xi = x3
-            yi = y1
-            dx4 = 0
-        else:
-            # calculate x coord of the intercept of the edge and the radius perpendicular to the edge
-            # numerator
-            xin = (y3 * (x2 - x1) - ((x2 - x1) ** 2 / y1 - y2)
-                   * x3 - (x1 * y2 - x2 * y1))
-            # denominator
-            xid = ((y1 - y2) - ((x2 - x1) ** 2 / (y1 - y2)))
-            xi = xin / xid
-
-            # calculate y coord of the intercept of the edge and the radius
-            # perpendicular to the edge
-            yi = ((x2 - x1) / (y1 - y2)) * x3 - \
-                y3 - ((x2 - x1) / (y1 - y2)) * xi
-
-            dx4 = -1 * ((y1 - y2) / (x2 - x1))
-
-        # calculate length of perpendicular line segment from radius to the
-        # edge
-        seg_len = distance(xi, yi, x3, y3)
-
-        d_arc1 = math.tan(heading + arc_len_deg / 2)
-        d_arc2 = math.tan(heading - arc_len_deg / 2)
-
-        # if the line segment from the radius perpendicular to the edge is less than the total length of the radius,
-        # the rectangle intercepts with the rectangle
-        # need to also check that the intersect point is actually between the two endpoints of the edge
-        # and to adjust for the arc, need to check the slope of the perp. line is between the two slopes of the bounding
-        # lines of the arc
-        if (seg_len < radius and ((distance(x1, y1, x3, y3) < radius or distance(
-                x2, y2, x3, y3) < radius) or x1 <= xi <= x2 and y1 <= yi <= y2)):
-            # TODO add check for arc, currently only checks circle
 
             return True
 
@@ -156,21 +85,38 @@ def distance(x1, y1, x2, y2):
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
 
 
-def arc_splitting(center, radius, hitbox, heading, arc_len_deg):
-    arc =
+def intersect_arc(center, radius, hitbox, heading, arc_len_deg):
+    # define left most point of arc
+    a1 = (radius * math.cos(math.radians(90 - arc_len_deg / 2)) + center[0],
+          radius * math.sin(math.radians(90 - arc_len_deg / 2)) + center[1])
+    # define right most point of arc
+    a2 = (radius * math.cos(math.radians(90 + arc_len_deg / 2)) + center[0],
+          radius * math.sin(math.radians(90 + arc_len_deg / 2)) + center[1])
+    # define left half of arc
+    arc1 = [
+        center,
+        (radius * math.cos(math.radians(arc_len_deg / 2)) + center[0],
+         radius * math.sin(math.radians(arc_len_deg / 2)) + center[1]),
+        a1
+    ]
+    # define right half of arc
+    arc2 = [
+        center,
+        (radius * math.cos(math.radians(arc_len_deg / 2)) + center[0],
+         radius * math.sin(math.radians(arc_len_deg / 2)) + center[1]),
+        a2
+    ]
+    arcs = [arc1, arc2]
 
-def minkowski_arc_intersection(center, radius, hitbox, heading, arc_len_deg):
-    a1 = (radius * math.cos(math.radians(90 - arc_len_deg / 2)) + center[0], radius * math.sin(math.radians(90 - arc_len_deg / 2)) + center[1])
-    arc1 = [center, (radius * math.cos(math.radians(arc_len_deg / 2)) + center[0], radius * math.sin(math.radians(arc_len_deg / 2)) + center[1])]
-
-
-    dilation = set()
-    r = hitbox.bottomRight
-    rp = (distance, )
-    for pa in arc:
-        for pr in hitbox:
-            dilation.add((pa[0] + pr[0], pa[1] + pr[1]))
-    if
+    for arc in arcs:
+        dilation = set()
+        r = hitbox.bottomRight
+        for pa in arc:
+            for pr in hitbox:
+                dilation.add((pa[0] + pr[0], pa[1] + pr[1]))
+        if is_point_in_path(r[0], r[1], list(dilation)):
+            return True
+    return False
 
 
 def is_point_in_path(x: int, y: int, poly) -> bool:
