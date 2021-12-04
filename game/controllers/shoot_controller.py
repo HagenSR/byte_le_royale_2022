02f3c42d24b281d6fb478225679154348f807025
erickbickler <erickbickler@gmail.com>
2021-12-04 00:34:50 -0600

    fix apply damage method to account for multiple pellets

--- game/controllers/shoot_controller.py
+++ game/controllers/shoot_controller.py
@@ -7,22 +7,22 @@ from game.controllers.controller import Controller
 from game.common.enums import *
 
 
-def apply_damage(collision_object, ray, gun, game_board):
+def apply_damage(collision_object, ray, gun, game_board, pellet_count):
     if collision_object is None:
         # no collision
         return
     elif isinstance(ray.collision, Shooter):
         collision_object.health -= round(
-            gun.damage / GameStats.shot_pattern_multi_pellet_count)
+            gun.damage / pellet_count)
     elif isinstance(ray.collision, Wall):
         collision_object.health -= round(
-            gun.damage / GameStats.shot_pattern_multi_pellet_count)
+            gun.damage / pellet_count)
         if collision_object.health <= 0:
             game_board.partition.remove_object(
                 collision_object)
     elif isinstance(collision_object, Door):
         collision_object.health -= round(
-            gun.damage / GameStats.shot_pattern_multi_pellet_count)
+            gun.damage / pellet_count)
         if collision_object.health <= 0:
             game_board.partition.remove_object(
                 collision_object)
@@ -39,13 +39,13 @@ class ShootController(Controller):
                 ray = get_gun_ray_collision(client, game_board)
                 game_board.ray_list.append(ray)
                 collision_object = ray.collision
-                apply_damage(collision_object, ray, gun, game_board)
+                apply_damage(collision_object, ray, gun, game_board, 1)
             elif gun.pattern == ShotPattern.multi:
                 for i in range(gun.fire_rate):
                     ray = get_gun_ray_collision(client, game_board)
                     game_board.ray_list.append(ray)
                     collision_object = ray.collision
-                    apply_damage(collision_object, ray, gun, game_board)
+                    apply_damage(collision_object, ray, gun, game_board, 1)
             elif gun.pattern == ShotPattern.spread:
                 arc_diff = (GameStats.shot_pattern_multi_arc /
                             GameStats.shot_pattern_multi_pellet_count)
@@ -66,4 +66,4 @@ class ShootController(Controller):
                     )
                     game_board.ray_list.append(ray)
                     collision_object = ray.collision
-                    apply_damage(collision_object, ray, gun, game_board)
+                    apply_damage(collision_object, ray, gun, game_board, GameStats.shot_pattern_multi_pellet_count)
