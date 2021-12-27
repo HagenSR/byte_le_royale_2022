implement rotation fixes in hitbox, rollback changes in check_collision and partition_grid check_overlap

game/utils/collision_detection.py
@@ -1,149 +1,11 @@
 import math
 
 
-# My work
-
-
 def check_collision(hitbox_one, hitbox_two):
-    hitbox1_corners = [
-        hitbox_one.top_left,
-        hitbox_one.top_right,
-        hitbox_one.bottom_right,
-        hitbox_one.bottom_left]
-    hitbox2_corners = [
-        hitbox_two.top_left,
-        hitbox_two.top_right,
-        hitbox_two.bottom_right,
-        hitbox_two.bottom_left]
-
-    for i in range(4):
-        for j in range(4):
-            if (
-                hitbox1_corners[i][0] < hitbox2_corners[(1 + j) % 4][0] and
-                hitbox1_corners[(1 + i) % 4][0] > hitbox2_corners[j][0] and
-                hitbox1_corners[i][1] < hitbox2_corners[(3 + j) % 4][1] and
-                hitbox1_corners[(2 + i) % 4][1] > hitbox2_corners[(1 + j) % 4][1]
-            ):
-                return True
-    return False
-
-    # return (hitbox_one.top_left[0] < hitbox_two.top_right[0] and
-    #         hitbox_one.top_right[0] > hitbox_two.top_left[0] and
-    #         hitbox_one.top_left[1] < hitbox_two.bottom_left[1] and
-    #         hitbox_one.bottom_right[1] > hitbox_two.top_right[1])
-
-
-###############################################################################
-
-
-# def collide_rect_hb(hb1, hb2):
-#     p1 = [hb1.topRight, hb1.topLeft, hb1.bottomLeft,
-#           hb1.bottomRight]
-#     p2 = [hb2.topRight, hb2.topLeft, hb2.bottomLeft,
-#           hb2.bottomRight]
-#
-#     """
-#     Return True and the MPV if the shapes collide. Otherwise, return False and
-#     None.
-#
-#     p1 and p2 are lists of ordered pairs, the vertices of the polygons in the
-#     counterclockwise direction.
-#     """
-#
-#     # p1 = [np.array(v, 'float64') for v in p1]
-#     # p2 = [np.array(v, 'float64') for v in p2]
-#
-#     edges = edges_of(p1)
-#     edges += edges_of(p2)
-#     orthogonals = [orthogonal(e) for e in edges]
-#
-#     push_vectors = []
-#     for o in orthogonals:
-#         separates, pv = is_separating_axis(o, p1, p2)
-#
-#         if separates:
-#             # they do not collide and there is no push vector
-#             return False
-#         else:
-#             push_vectors.append(pv)
-#
-#     # they do collide and the push_vector with the smallest length is the MPV
-#     mpv = min(push_vectors, key=(lambda v: np.dot(v, v)))
-#
-#     # assert mpv pushes p1 away from p2
-#     d = centers_displacement(p1, p2)  # direction from p1 to p2
-#     if np.dot(d, mpv) > 0:  # if it's the same direction, then invert
-#         mpv = -mpv
-#
-#     # return True, mpv
-#     return True
-#
-#
-# def centers_displacement(p1, p2):
-#     """
-#     Return the displacement between the geometric center of p1 and p2.
-#     """
-#     # geometric center
-#     c1 = np.mean(np.array(p1), axis=0)
-#     c2 = np.mean(np.array(p2), axis=0)
-#     return c2 - c1
-#
-#
-# def edges_of(vertices):
-#     """
-#     Return the vectors for the edges of the polygon p.
-#
-#     p is a polygon.
-#     """
-#     edges = []
-#     N = len(vertices)
-#
-#     for i in range(N):
-#         edge = [vertices[(i + 1) % N][j] - vertices[i][j] for j in range(len(vertices[i]))]
-#         edges.append(edge)
-#
-#     return edges
-#
-#
-# def orthogonal(v):
-#     """
-#     Return a 90 degree clockwise rotation of the vector v.
-#     """
-#     return np.array([-v[1], v[0]])
-#
-#
-# def is_separating_axis(o, p1, p2):
-#     """
-#     Return True and the push vector if o is a separating axis of p1 and p2.
-#     Otherwise, return False and None.
-#     """
-#     min1, max1 = float('+inf'), float('-inf')
-#     min2, max2 = float('+inf'), float('-inf')
-#
-#     for v in p1:
-#         projection = np.dot(v, o)
-#
-#         min1 = min(min1, projection)
-#         max1 = max(max1, projection)
-#
-#     for v in p2:
-#         projection = np.dot(v, o)
-#
-#         min2 = min(min2, projection)
-#         max2 = max(max2, projection)
-#
-#     if max1 >= min2 and max2 >= min1:
-#         d = min(max2 - min1, max1 - min2)
-#         # push a bit more than needed so the shapes do not overlap in future
-#         # tests due to float precision
-#         d_over_o_squared = d / np.dot(o, o) + 1e-10
-#         pv = d_over_o_squared * o
-#         return False, pv
-#     else:
-#         return True, None
-
-
-##########################################################################
+    return (hitbox_one.topLeft[0] < hitbox_two.topRight[0] and
+            hitbox_one.topRight[0] > hitbox_two.topLeft[0] and
+            hitbox_one.topLeft[1] < hitbox_two.bottomLeft[1] and
+            hitbox_one.bottomRight[1] > hitbox_two.topRight[1])
 
 
 def arc_intersect_rect(center, radius, arc_len_degree, hitbox, heading):
@@ -162,11 +24,6 @@ def point_in_hitbox(x, y, hitbox):
     return (hitbox.bottom_left[0] <= x <= hitbox.top_right[0] and
             hitbox.top_right[1] <= y <= hitbox.bottom_left[1])
 
-
-# def hitbox_angle(player_center, hitbox)
-
-
-# def intersect_arc(center, radius, hitbox, heading, arc_len_deg):
 
 def intersect_circle(center, radius, hitbox):
     edges = [