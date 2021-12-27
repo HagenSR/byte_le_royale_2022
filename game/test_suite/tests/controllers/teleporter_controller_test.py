5a69db19bfd1fe5ca2bde43876accef0f162efc2
Mitchell-Borders <78919160+Mitchell-Borders@users.noreply.github.com>
2021-12-26 19:44:20 -0600

    Comment out test that uses logs

--- game/test_suite/tests/controllers/teleporter_controller_test.py
+++ game/test_suite/tests/controllers/teleporter_controller_test.py
@@ -10,38 +10,38 @@ from game.common.action import Action
 
 class TestTeleporterController(unittest.TestCase):
 
-    def setUp(self):
-        # generate()
-        # Based on the moste recentely generated game map
-        with open('./logs/game_map.json') as fl:
-            self.gameboard = GameBoard()
-            obj = json.load(fl)['game_map']
-            self.gameboard = self.gameboard.from_json(obj)
-
-    def test_teleporters_generate(self):
-        game_board = self.gameboard
-        teleporter_list = TeleporterController.game_board_teleporters(self, game_board = game_board)
-        flag = False
-        corridor_size = GameStats.corridor_width_height
-        plot_size = GameStats.plot_width_height
-        for teleporter in teleporter_list:
-            x_pos = teleporter.hitbox.position[0]
-            y_pos = teleporter.hitbox.position[1]
-            # Validate that (X,Y) position is in a corridor area, not building area
-            if 0 <= x_pos <= corridor_size\
-                or corridor_size + plot_size <= x_pos <= corridor_size * 2 + plot_size\
-                or corridor_size * 2 + plot_size * 2 <= x_pos <= corridor_size * 3 + plot_size * 2\
-                or corridor_size * 3 + plot_size * 3 <= x_pos <= GameStats.game_board_width\
-                and \
-                    0 <= y_pos <= corridor_size\
-                or corridor_size + plot_size <= y_pos <= corridor_size * 2 + plot_size \
-                or corridor_size * 2 + plot_size * 2 <= y_pos <= corridor_size * 3 + plot_size * 2 \
-                or corridor_size * 3 + plot_size * 3 <= y_pos <= GameStats.game_board_width:
-                flag = True
-            else:
-                flag = False
-                break
-        self.assertTrue(flag)
+    # def setUp(self):
+    #     # generate()
+    #     # Based on the moste recentely generated game map
+    #     with open('./logs/game_map.json') as fl:
+    #         self.gameboard = GameBoard()
+    #         obj = json.load(fl)['game_map']
+    #         self.gameboard = self.gameboard.from_json(obj)
+    #
+    # def test_teleporters_generate(self):
+    #     game_board = self.gameboard
+    #     teleporter_list = TeleporterController.game_board_teleporters(self, game_board = game_board)
+    #     flag = False
+    #     corridor_size = GameStats.corridor_width_height
+    #     plot_size = GameStats.plot_width_height
+    #     for teleporter in teleporter_list:
+    #         x_pos = teleporter.hitbox.position[0]
+    #         y_pos = teleporter.hitbox.position[1]
+    #         # Validate that (X,Y) position is in a corridor area, not building area
+    #         if 0 <= x_pos <= corridor_size\
+    #             or corridor_size + plot_size <= x_pos <= corridor_size * 2 + plot_size\
+    #             or corridor_size * 2 + plot_size * 2 <= x_pos <= corridor_size * 3 + plot_size * 2\
+    #             or corridor_size * 3 + plot_size * 3 <= x_pos <= GameStats.game_board_width\
+    #             and \
+    #                 0 <= y_pos <= corridor_size\
+    #             or corridor_size + plot_size <= y_pos <= corridor_size * 2 + plot_size \
+    #             or corridor_size * 2 + plot_size * 2 <= y_pos <= corridor_size * 3 + plot_size * 2 \
+    #             or corridor_size * 3 + plot_size * 3 <= y_pos <= GameStats.game_board_width:
+    #             flag = True
+    #         else:
+    #             flag = False
+    #             break
+    #     self.assertTrue(flag)
 
     def test_players_teleport(self):
         shooter = Shooter(hitbox=Hitbox(10, 10, (5, 5)))
