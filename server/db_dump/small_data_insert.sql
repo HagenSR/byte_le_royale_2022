309895e32dffa4e08a4896a9b62982fee33275cd
Sean Hagen <sean.r.hagen@gmail.com>
2022-01-08 17:09:52 -0600

    Fixed leaderboard commands

--- server/db_dump/small_data_insert.sql
+++ server/db_dump/small_data_insert.sql
@@ -13,3 +13,33 @@ VALUES
     ('MSUM'),
     ('UND'),
     ('Other');
+
+SELECT
+    results.grouprunid,
+    dense_rank() OVER (
+        ORDER BY
+            total_wins DESC
+    ) as place,
+    results.team_name,
+    results.uni_name,
+    results.start_run,
+    results.total_wins,
+    results.launcher_version
+FROM
+    (
+        SELECT
+            (get_leaderboard(true, group_run.group_run_id)).* as leaderboard
+        FROM
+            group_run
+    ) as results
+WHERE
+    results.team_name = (
+        Select
+            team_name
+        FROM
+            team
+        WHERE
+            team.team_id = teamid
+    )
+ORDER BY
+    leaderboards.group_run_id DESC;
