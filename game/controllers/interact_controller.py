39a8d9829fe9f9b0657f043c2a38d8276e22efb3
amanda-f-ndsu <71937870+amanda-f-ndsu@users.noreply.github.com>
2022-01-07 19:49:07 -0600

    Create interact controller (#146)
    * pulled from shop controlle branch, changed action enum name 'pickup' to 'interact', and started on interact controller + unit test cases
    
    * continued work on controller and unit tests
    
    * fixed errors in first unit test
    
    * got current unit tests to work
    
    * started working on door tests. bugs need to be fixed
    
    * fix circular imports
    
    * finished unit tests for doors
    
    * allowed player to open door within range, added door range in gamestats
    
    * finished unit tests and added interact controller to master controller
    
    * Automated autopep8 fixes
    
    * made fixes to interact controller logic
    
    * change find_doors to circle
    
    * Automated autopep8 fixes
    
    Co-authored-by: Erick Bickler <erickbickler@gmail.com>
    Co-authored-by: Github Actions <action@github.com>

--- game/controllers/interact_controller.py
+++ game/controllers/interact_controller.py
@@ -1,12 +1,13 @@
+import math
+
 from game.controllers.controller import Controller
 from game.common.stats import GameStats
 from game.common.items.upgrade import Upgrade
 from game.common.door import Door
 from game.common.items.money import Money
+from game.utils import collision_detection
 from game.utils.collision_detection import check_collision
 from game.common.moving.shooter import Shooter
-from game.common.action import Action
-from game.common.game_board import GameBoard
 from game.common.enums import *
 
 
@@ -18,32 +19,58 @@ class InteractController(Controller):
     def handle_actions(self, client, world):
         object_target = False
         if client.action._chosen_action is ActionType.interact:
+
+            # gets list of objects in the same partition as the player
             object_list = world["game_board"].partition.get_partition_objects(client.shooter.hitbox.position[0],
                                                                               client.shooter.hitbox.position[1])
-            for obj in object_list:
-                if check_collision(client.shooter.hitbox, obj.hitbox) and not isinstance(obj, Shooter):
-                    object_target = obj
-            print(object_target)
-            if not object_target:
-                raise ValueError("There is no object it interact with.")
+
+            # partition grid checks to see if any objects collide with the player's hitbox
+            object_target = world["game_board"].partition.find_object_hitbox(client.shooter.hitbox)
             if isinstance(object_target, Upgrade):
                 self.interact_upgrade(client, world, object_target)
-            elif isinstance(object_target, Door):
-                self.interact_door(client, object_target)
             elif isinstance(object_target, Money):
                 self.interact_money(client, world, object_target)
             else:
-                pass
+                # if there are no objects beneath the player, the controller checks if there are nearby doors
+                # to interact with.
+                object_target = self.find_doors(client.shooter.hitbox.middle, world["game_board"].partition)
+                if isinstance(object_target, Door):
+                    self.interact_door(object_target)
+                else:
+                    raise ValueError("There is no object to interact with.")
 
+    # removes upgrade from beneath the player and adds the upgrade to their inventory
     def interact_upgrade(self, client, world, upgrade):
         if client.shooter.has_empty_slot('upgrades'):
             client.shooter.append_inventory(upgrade)
             world["game_board"].partition.remove_object(upgrade)
 
+    # removes money from beneath the player and adds the money amount to their stats
     def interact_money(self, client, world, money):
         client.shooter.money = client.shooter.money + money.amount
         world["game_board"].partition.remove_object(money)
 
+    # changes door state to open and allows the users to walk through it
     def interact_door(self, door):
         if not door.open_state:
             door.open_state = True
+            door.collidable = False
+
+    def find_doors(self, player_coords, partition):
+        objects = []
+        for x in range(0, GameStats.game_board_width, partition.partition_width):
+            for y in range(0, GameStats.game_board_height, partition.partition_height):
+                if collision_detection.intersect_circle(
+                        player_coords,
+                        GameStats.max_allowed_dist_from_door,
+                        partition.get_partition_hitbox(x, y)):
+
+                    objects.extend(partition.get_partition_objects_index(x, y))
+
+        filter(lambda obj: isinstance(obj, Door), objects)
+        return min(objects,
+                   key=lambda obj:
+                   collision_detection.distance(player_coords[0],
+                                                player_coords[1],
+                                                obj.hitbox.position[0],
+                                                obj.hitbox.position[1]))
--- game/test_suite/tests/controllers/test_interact_controller.py
+++ game/test_suite/tests/controllers/test_interact_controller.py
@@ -7,6 +7,7 @@ from game.common.hitbox import Hitbox
 from game.common.items.upgrade import Upgrade
 from game.common.moving.shooter import Shooter
 from game.common.player import Player
+from game.common.stats import GameStats
 from game.controllers.interact_controller import InteractController
 from game.common.enums import *
 from game.common.items.money import Money
@@ -36,6 +37,14 @@ class TestInteractController(unittest.TestCase):
                           self.interactController.handle_actions,
                           self.myPlayer, self.world_data)
 
+        self.myPlayer = Player(shooter=Shooter(
+            0, 0, Hitbox(
+                10, 10, (10, 10), 0)))
+        self.myPlayer.action._chosen_action = ActionType.interact
+        self.interactController = InteractController()
+        self.world_data = {'game_board': GameBoard()}
+
+    # testing controller when no objects/doors are present
     def test_interact_object_invalid(self):
         self.myPlayer.shooter.hitbox.position = (50, 50)
         self.world_data["game_board"].partition.add_object(
@@ -44,6 +53,7 @@ class TestInteractController(unittest.TestCase):
                           self.interactController.handle_actions,
                           self.myPlayer, self.world_data)
 
+    # interacting with upgrade beneath player
     def test_pickup_upgrade(self):
         an_item = Upgrade(Hitbox(
             10, 10, (45, 50), 0))
@@ -58,6 +68,7 @@ class TestInteractController(unittest.TestCase):
         self.interactController.handle_actions(self.myPlayer, self.world_data)
         self.assertFalse(self.myPlayer.shooter.has_empty_slot('upgrades'))
 
+    # interacting with money beneath player
     def test_pickup_money(self):
         self.myPlayer.shooter.money = 150
         old_money = self.myPlayer.shooter.money
@@ -68,6 +79,159 @@ class TestInteractController(unittest.TestCase):
             self.myPlayer.shooter)
         self.interactController.handle_actions(self.myPlayer, self.world_data)
         self.assertTrue(self.myPlayer.shooter.money, (old_money + money_object.amount))
+
+    # attempting to interact with door out of range
+    def test_interact_door_too_far(self):
+        self.myPlayer.shooter.hitbox.position = (50, 50)
+        door_object = Door(Hitbox(3, 10, (80, 50)))
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object)
+        self.assertRaises(ValueError,
+                          self.interactController.handle_actions,
+                          self.myPlayer, self.world_data)
+
+    # standing beside door left side
+    def test_beside_open_door_1(self):
+        self.myPlayer.shooter.hitbox.position = (50, 50)
+        door_object = Door(Hitbox(3, 10, (61, 50)))
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertTrue(door_object.open_state)
+
+    # standing beside door right side
+    def test_beside_open_door_2(self):
+        self.myPlayer.shooter.hitbox.position = (50, 50)
+        door_object = Door(Hitbox(3, 10, (49, 50)))
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertTrue(door_object.open_state)
+
+    # standing beside door top
+    def test_beside_open_door_3(self):
+        self.myPlayer.shooter.hitbox.position = (50, 50)
+        door_object = Door(Hitbox(10, 3, (50, 46)))
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertTrue(door_object.open_state)
+
+    # standing beside door bottom
+    def test_beside_open_door_4(self):
+        self.myPlayer.shooter.hitbox.position = (50, 50)
+        door_object = Door(Hitbox(10, 3, (50, 61)))
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertTrue(door_object.open_state)
+
+    # two doors within range, should detect both doors but only open door 1
+    def test_correct_door_opened_1(self):
+        self.myPlayer.shooter.hitbox.position = (50, 50)
+        door_object_1 = Door(Hitbox(3, 10, (61, 50)))  # is closest door
+        door_object_2 = Door(Hitbox(10, 3, (50, 62)))
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object_1)
+        self.world_data["game_board"].partition.add_object(
+            door_object_2)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertTrue(door_object_1.open_state)
+        self.assertFalse(door_object_2.open_state)
+
+    # three doors within range, should detect all doors but only open door 3
+    def test_correct_door_opened_2(self):
+        self.myPlayer.shooter.hitbox.position = (70, 75)
+        door_object_1 = Door(Hitbox(3, 10, (83, 75)))
+        door_object_2 = Door(Hitbox(10, 3, (70, 89)))
+        door_object_3 = Door(Hitbox(10, 3, (70, 70)))  # is closest door
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object_1)
+        self.world_data["game_board"].partition.add_object(
+            door_object_2)
+        self.world_data["game_board"].partition.add_object(
+            door_object_3)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertFalse(door_object_1.open_state)
+        self.assertFalse(door_object_2.open_state)
+        self.assertTrue(door_object_3.open_state)
+
+    # two doors within range, should detect both doors but only open door 2
+    def test_correct_door_opened_3(self):
+        self.myPlayer.shooter.hitbox.position = (30, 30)
+        door_object_1 = Door(Hitbox(3, 10, (44, 30)))
+        door_object_2 = Door(Hitbox(3, 10, (26, 30)))  # is closest door
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object_1)
+        self.world_data["game_board"].partition.add_object(
+            door_object_2)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertFalse(door_object_1.open_state)
+        self.assertTrue(door_object_2.open_state)
+
+    # testing max distance in the down direction
+    def test_door_max_dist_1(self):
+        self.myPlayer.shooter.hitbox.position = (50, 50)
+        door_object = Door(Hitbox(10, 3, (50, self.myPlayer.shooter.hitbox.middle[1] +
+                                          GameStats.max_allowed_dist_from_door)))
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertTrue(door_object.open_state)
+
+    # testing max distance to the right
+    def test_door_max_dist_2(self):
+        self.myPlayer.shooter.hitbox.position = (50, 50)
+        door_object = Door(Hitbox(3, 10, (self.myPlayer.shooter.hitbox.middle[0] +
+                                          GameStats.max_allowed_dist_from_door, 50)))
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertTrue(door_object.open_state)
+
+    # testing max distance in the up direction
+    def test_door_max_dist_3(self):
+        self.myPlayer.shooter.hitbox.position = (50, 50)
+        door_object = Door(Hitbox(10, 3, (50, self.myPlayer.shooter.hitbox.middle[1] -
+                                          GameStats.max_allowed_dist_from_door)))
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertTrue(door_object.open_state)
+
+    # testing max distance in the left direction
+    def test_door_max_dist_4(self):
+        self.myPlayer.shooter.hitbox.position = (50, 50)
+        door_object = Door(Hitbox(3, 10, (self.myPlayer.shooter.hitbox.middle[0] -
+                                          GameStats.max_allowed_dist_from_door, 50)))
+        self.world_data["game_board"].partition.add_object(
+            self.myPlayer.shooter)
+        self.world_data["game_board"].partition.add_object(
+            door_object)
+        self.interactController.handle_actions(self.myPlayer, self.world_data)
+        self.assertTrue(door_object.open_state)
 
     if __name__ == '__main__':
         unittest.main
