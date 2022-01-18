281a147513fb40a1f3341acc96a8441941b086e4
Sean Hagen <49661807+HagenSR@users.noreply.github.com>
2022-01-17 18:56:02 -0600

    Fix type annotations (#180)

--- game/utils/player_utils.py
+++ game/utils/player_utils.py
@@ -13,10 +13,10 @@ def distance(x1: float, y1: float, x2: float, y2: float) -> float:
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
 
 
-def distance_tuples(coord_tuple1: tuple[float, float], coord_tuple2: tuple[float, float]) -> float:
+def distance_tuples(coord_tuple1: "tuple[float, float]", coord_tuple2: "tuple[float, float]") -> float:
     return distance(coord_tuple1[0], coord_tuple1[1], coord_tuple2[0], coord_tuple2[1])
 
 
-def angle_to_point(player, coord_tuple: tuple[float, float]) -> float:
+def angle_to_point(player, coord_tuple: "tuple[float, float]") -> float:
     return int(math.degrees(math.acos(abs(player.hitbox.position[1] - coord_tuple[1]) /
                                       distance_tuples(player.hitbox.position, coord_tuple))))
