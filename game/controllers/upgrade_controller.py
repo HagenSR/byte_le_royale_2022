20e09ad9736534849b6878cd6606cff502294ebe
amanda-f-ndsu <71937870+amanda-f-ndsu@users.noreply.github.com>
2022-01-22 20:16:10 -0600

    fixed NoneType exception coming from upgrade controller

--- game/controllers/upgrade_controller.py
+++ game/controllers/upgrade_controller.py
@@ -9,13 +9,14 @@ class UpgradeController(Controller):
 
     def handle_actions(self, client):
         for upgrade in client.shooter.inventory['upgrades']:
-            if upgrade.applied:
-                continue
-            if upgrade.upgrade_enum == Upgrades.armor:
-                client.shooter.armor = GameStats.upgrade_stats["armor_damage_reduction"]
-            elif upgrade.upgrade_enum == Upgrades.movement_boots:
-                client.shooter.max_speed *= 1 + GameStats.upgrade_stats["movement_boost"]
-            elif upgrade.upgrade_enum == Upgrades.backpack:
-                for i in range(GameStats.upgrade_stats["backpack_slot_increase"]):
-                    client.shooter.add_new_slot("consumables")
-            upgrade.applied = True
+            if upgrade is not None:
+                if upgrade.applied:
+                    continue
+                if upgrade.upgrade_enum == Upgrades.armor:
+                    client.shooter.armor = GameStats.upgrade_stats["armor_damage_reduction"]
+                elif upgrade.upgrade_enum == Upgrades.movement_boots:
+                    client.shooter.max_speed *= 1 + GameStats.upgrade_stats["movement_boost"]
+                elif upgrade.upgrade_enum == Upgrades.backpack:
+                    for i in range(GameStats.upgrade_stats["backpack_slot_increase"]):
+                        client.shooter.add_new_slot("consumables")
+                upgrade.applied = True
